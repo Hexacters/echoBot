e4e1ab245c28a5a25d934e2f74bcd3994673e279
Venkat <39988591+SMIFreshers@users.noreply.github.com>
2019-01-28 16:07:29 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const http = require('http');
-const https = require('https');
 const port = process.env.PORT || 8000;
-
 const restService = express();
+
+var translate = require('./translate/index');
 
 restService.use(
   bodyParser.urlencoded({
@@ -14,21 +13,20 @@ restService.use(
   })
 );
 
-var tokens = {
-  translate : "trnsl.1.1.20190121T114853Z.bb13b14c2fb8537f.ff83b8ea03a6c04e712181d5152536e30d16c5f5"
-}
-
+let defaultValues = ["doing", "hello", "hai", "hi", "date", "time", "status", "Hexa", "about you"];
 restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
-  //var speech = req.body.tr ? req.body.tr.toLowerCase() : "Seems like some problem. Speak again.";
-  console.log(req.body);
-  var speech =
-      req.body.queryResult &&
-      req.body.queryResult.parameters &&
-      req.body.queryResult.parameters.echoText
-        ? req.body.queryResult.parameters.echoText
-        : "Seems like some problem. Speak again.";
+  var speech = req.body.tr ? req.body.tr.toLowerCase() : "Seems like some problem. Speak again.";
+  // console.log(req.body.originalDetectIntentRequest.payload.data.user.name);
+  // var speech =
+  //     req.body.queryResult &&
+  //     req.body.queryResult.parameters &&
+  //     req.body.queryResult.parameters.echoText
+  //       ? req.body.queryResult.parameters.echoText
+  //       : "Seems like some problem. Speak again.";
+
+  var element = '';
   let translateArray = ["translate", "say"];
   let conjectionArray = ["in"];
   if (translateArray.some(substring=>speech.includes(substring)) && conjectionArray.some(substring=>speech.includes(substring))) {
@@ -46,63 +44,35 @@ restService.post("/echo", function(req, res) {
     });
 
     speech = speech.replace(lang, '').trim();
-
-    //get Language Code
-    var url = "https://translate.yandex.net/api/v1.5/tr.json/getLangs?"
-    var params = "key=" +tokens.translate + "&ui=en"
-    https.get(url + params, (responseFromAPI) => {
-      let completeResponse = '';
-      responseFromAPI.on('data', (chunk) => {
-          completeResponse += chunk;
-      });
-      responseFromAPI.on('end', () => {
-        const langC = JSON.parse(completeResponse);
-        var langCodes = langC.langs;
-        var langCode = '';
-          for(var key in langCodes){
-            if (langCodes[key].toLowerCase() === lang.toLowerCase()) {
-              langCode = key;
-            }
-          }
-
-          //Translate Process
-          url = "https://translate.yandex.net/api/v1.5/tr.json/translate?"
-          params = "key=" +tokens.translate + " &text=" + speech + "&lang=" + langCode
-          https.get(url + params, (responseFromAPI) => {
-            let completeResponse = '';
-            responseFromAPI.on('data', (chunk) => {
-                completeResponse += chunk;
-            });
-            responseFromAPI.on('end', () => {
-              const dataa = JSON.parse(completeResponse);
-              speech = dataa.text;
-              if (!dataa) {
-                speech = "Sorry I cant Understand Your Translate Language";
+    console.log(lang, speech);
+    //Translate
+    translate(lang, speech).then(function(speech){
+      if (!speech) {
+        speech = "Sorry! i can't Understand!.. :("
+      }
+      return res.json({
+          fulfillmentText:speech,
+          fulfillmentMessages:[
+            {
+              text: {
+                  text: [
+                     speech
+                  ]
               }
-              return res.json({
-                  fulfillmentText:speech,
-                  fulfillmentMessages:[
-                    {
-                      text: {
-                          text: [
-                             speech
-                          ]
-                      }
-                    }
-                  ],
-                  source:"Copy Cat"
-              });
-            });
-          }, (error) => {
-            return null;
-          });
+            }
+          ],
+          source:"Copy Cat"
       });
-    }, (error) => {
-      return null;
     });
-
+    
   } else {
-      switch (speech) {
+      defaultValues.map(s => {
+        if (speech.includes(s)) {
+          element = s;
+        }
+      });
+      console.log(element);
+      switch (element) {
       //Speech Synthesis Markup Language 
       case "date":
         var datetime = new Date();
@@ -123,6 +93,29 @@ restService.post("/echo", function(req, res) {
         speech =
           '<speak>It is ' + hours + ':' + min + ' ' + ampm + ' now</speak>';
         break;
+
+      case "hai":
+      case "hello":
+      case "haii":
+        speech =
+          'Haii ' + req.body.originalDetectIntentRequest.payload.data.user.name + '...! :)';
+        break;
+
+      case "about you":
+      case "status":
+        if (req.body.originalDetectIntentRequest.source) {
+        speech = 
+          'Haii ' + req.body.originalDetectIntentRequest.payload.data.user.name + '..! :) \n' +
+          'Now i am in ' + req.body.originalDetectIntentRequest.source + '\n' +
+          'We are Talking in ' + 'Persnal Chat';
+        } else {
+          speech = 'I am online Now';
+        }
+        break;
+      case "doing":
+        speech = 
+          'I am doing Well what About You';
+        break;
       }
 
       return res.json({
@@ -136,181 +129,11 @@ restService.post("/echo", function(req, res) {
                 }
             }
         ],
-        source:"Copy Cat"
+        source:"Hexa"
       });
   }
 
-
-   
   
-});
-
-restService.post("/audio", function(req, res) {
-  var speech = "";
-  switch (req.body.result.parameters.AudioSample.toLowerCase()) {
-    //Speech Synthesis Markup Language 
-    case "music one":
-      speech =
-        '<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
-    case "music two":
-      speech =
-        '<speak><audio clipBegin="1s" clipEnd="3s" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
-    case "music three":
-      speech =
-        '<speak><audio repeatCount="2" soundLevel="-15db" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
-    case "music four":
-      speech =
-        '<speak><audio speed="200%" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
-    case "music five":
-      speech =
-        '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>';
-      break;
-    case "delay":
-      speech =
-        '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
-      break;
-    //https://www.w3.org/TR/speech-synthesis/#S3.2.3
-    case "cardinal":
-      speech = '<speak><say-as interpret-as="cardinal">12345</say-as></speak>';
-      break;
-    case "ordinal":
-      speech =
-        '<speak>I stood <say-as interpret-as="ordinal">10</say-as> in the class exams.</speak>';
-      break;
-    case "characters":
-      speech =
-        '<speak>Hello is spelled as <say-as interpret-as="characters">Hello</say-as></speak>';
-      break;
-    case "fraction":
-      speech =
-        '<speak>Rather than saying 24+3/4, I should say <say-as interpret-as="fraction">24+3/4</say-as></speak>';
-      break;
-    case "bleep":
-      speech =
-        '<speak>I do not want to say <say-as interpret-as="bleep">F&%$#</say-as> word</speak>';
-      break;
-    case "unit":
-      speech =
-        '<speak>This road is <say-as interpret-as="unit">50 foot</say-as> wide</speak>';
-      break;
-    case "verbatim":
-      speech =
-        '<speak>You spell HELLO as <say-as interpret-as="verbatim">hello</say-as></speak>';
-      break;
-    case "date one":
-      speech =
-        '<speak>Today is <say-as interpret-as="date" format="yyyymmdd" detail="1">2017-12-16</say-as></speak>';
-      break;
-    case "date two":
-      speech =
-        '<speak>Today is <say-as interpret-as="date" format="dm" detail="1">16-12</say-as></speak>';
-      break;
-    case "date three":
-      speech =
-        '<speak>Today is <say-as interpret-as="date" format="dmy" detail="1">16-12-2017</say-as></speak>';
-      break;
-    case "time":
-      speech =
-        '<speak>It is <say-as interpret-as="time" format="hms12">2:30pm</say-as> now</speak>';
-      break;
-    case "telephone one":
-      speech =
-        '<speak><say-as interpret-as="telephone" format="91">09012345678</say-as> </speak>';
-      break;
-    case "telephone two":
-      speech =
-        '<speak><say-as interpret-as="telephone" format="1">(781) 771-7777</say-as> </speak>';
-      break;
-    // https://www.w3.org/TR/2005/NOTE-ssml-sayas-20050526/#S3.3
-    case "alternate":
-      speech =
-        '<speak>IPL stands for <sub alias="indian premier league">IPL</sub></speak>';
-      break;
-  }
-  return res.json({
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
-  });
-});
-
-restService.post("/video", function(req, res) {
-  return res.json({
-    speech:
-      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
-    displayText:
-      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
-    source: "webhook-echo-sample"
-  });
-});
-
-restService.post("/slack-test", function(req, res) {
-  var slack_message = {
-    text: "Details of JIRA board for Browse and Commerce",
-    attachments: [
-      {
-        title: "JIRA Board",
-        title_link: "http://www.google.com",
-        color: "#36a64f",
-
-        fields: [
-          {
-            title: "Epic Count",
-            value: "50",
-            short: "false"
-          },
-          {
-            title: "Story Count",
-            value: "40",
-            short: "false"
-          }
-        ],
-
-        thumb_url:
-          "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
-      },
-      {
-        title: "Story status count",
-        title_link: "http://www.google.com",
-        color: "#f49e42",
-
-        fields: [
-          {
-            title: "Not started",
-            value: "50",
-            short: "false"
-          },
-          {
-            title: "Development",
-            value: "40",
-            short: "false"
-          },
-          {
-            title: "Development",
-            value: "40",
-            short: "false"
-          },
-          {
-            title: "Development",
-            value: "40",
-            short: "false"
-          }
-        ]
-      }
-    ]
-  };
-  return res.json({
-    speech: "speech",
-    displayText: "speech",
-    source: "webhook-echo-sample",
-    data: {
-      slack: slack_message
-    }
-  });
 });
 
 restService.listen(port, function() {
