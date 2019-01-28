5d44783510ca3ee3a0f65b2fadd8c318562bec5b
Venkat <39988591+SMIFreshers@users.noreply.github.com>
2019-01-28 12:21:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
   //var speech = req.body.tr ? req.body.tr.toLowerCase() : "Seems like some problem. Speak again.";
+  console.log(req.body);
   var speech =
       req.body.queryResult &&
       req.body.queryResult.parameters &&
